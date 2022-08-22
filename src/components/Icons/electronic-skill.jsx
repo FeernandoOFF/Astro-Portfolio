57d1c51d64b70e1886ca310be355fbe8314d5cad
Fernando <ferobregon03@gmail.com>
2022-08-22 10:55:50 +0100

    Project section

---
+++ src/components/Icons/electronic-skill.jsx
@@ -0,0 +1,27 @@
+import { h } from 'preact';
+
+export default function ElectronicIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="48"
+      height="48"
+      aria-hidden="true"
+      class="mb-6 text-pink-500 dark:text-pink-400 "
+    >
+      <path
+        d="M5 11a4 4 0 0 1 4-4h30a4 4 0 0 1 4 4v26a4 4 0 0 1-4 4H9a4 4 0 0 1-4-4V11Z"
+        fill="currentColor"
+        fill-opacity=".1"
+        stroke="currentColor"
+        stroke-width="2"
+      />
+      <path
+        d="M15 7v34"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+      />
+    </svg>
+  );
+}
